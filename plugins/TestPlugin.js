455446f46a7cdbce6dfba93ba75e4e9c17cfcdf9
kazaff <edisondik@gmail.com>
2017-03-05 22:29:27 -0500

    no message

--- plugins/TestPlugin.js
+++ plugins/TestPlugin.js
@@ -1,8 +1,8 @@
 function TestPlugin() {}
 TestPlugin.prototype.apply = function(PluginManager) {
 		PluginManager.plugin(PluginManager.EVENTS.TASK_COMPLETE, function(Context, cb) {
-			console.log('TASK_COMPLETE');
-			cb();
+			//console.log('TASK_COMPLETE');
+			cb();	// 一定要记得回调，否则请求会一直无法结束
     });
 }
 
